ab12e300cee37c32e0a55b1198bb866518b53643
duffn <3457341+duffn@users.noreply.github.com>
2023-03-12 15:30:25 -0600

    feat: Add yard docs for Comic, ComicList, and related classes

--- lib/harkness/models/comic_list.rb
+++ lib/harkness/models/comic_list.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 module Harkness
+  # The summary of a comic.
   class ComicSummary < Harkness::Base::Summary
   end
 
+  # A list of comics.
   class ComicList < Shale::Mapper
+    # @!attribute [r] available
+    # @return [Integer] the number of total available issues in this list. Will always be greater than
+    #   or equal to the "returned" value.
     attribute :available, Shale::Type::Integer
+    # @!attribute [r] collectionURI
+    # @return [String] the path to the full list of issues in this collection.
     attribute :collectionURI, Shale::Type::String
+    # @!attribute [r] collectionURI
+    # @return [Array<Harkness::ComicSummary>] the list of returned issues in this collection.
     attribute :items, Harkness::ComicSummary, collection: true
   end
 end
